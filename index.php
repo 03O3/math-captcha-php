69468f780806f6db5f2bd4103e4b4a40667e4827
Marcus <89999325+EncryRose@users.noreply.github.com>
2022-03-02 12:45:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,7 @@ $_SESSION['captcha'] = [
 <h1>Test captcha</h1>
 
 <form action="vendor/captcha_handler.php" method="POST">
-  <label for="captcha_server"><?php echo $numOne ." + ". $numTwo; ?>?</label><br>
-  <input type="text" id="captcha_user" name="captcha_input"><br><br>
+  <input type="text" id="captcha_user" name="captcha_input" placeholder="<?php echo $numOne ." + ". $numTwo; ?>?"><br><br>
   <input type="submit" value="Submit">
 </form>
 
